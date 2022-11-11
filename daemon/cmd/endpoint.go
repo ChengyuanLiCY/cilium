ChengyuanLiCY/cilium

commit bdb1b6308ef3a97c98a46806f8265c64b3c81176

daemon: Introduce endpointMetadataFetcher
endpointMetadataFetcher knows how to fetch Kubernetes metadata for
endpoints. Currently, there are two implementations:

1) Fetching from the K8s watcher store (cached)
2) Fetching directly from K8s itself (uncached)

(1) was the default method before this commit. With this commit, (2) is
possible when high-scale ipcache mode is enabled. This is because with
the aforementioned option enabled, the pod watcher is disabled, meaning
the store will be empty. In other wods, there's no pod metadata that's
been cached, so we must fetch it directly during endpoint restoration.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

daemon/cmd/endpoint.go
@@ -13,8 +13,10 @@ import (
 	"sync"
 	"time"
 
+	slimclientset "github.com/cilium/cilium/pkg/k8s/slim/k8s/client/clientset/versioned"
 	"github.com/go-openapi/runtime/middleware"
 	"github.com/sirupsen/logrus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/cilium/cilium/api/v1/models"
 	. "github.com/cilium/cilium/api/v1/server/restapi/endpoint"
@@ -28,6 +30,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
+	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/labelsfilter"
 	"github.com/cilium/cilium/pkg/lock"
@@ -170,11 +173,7 @@ func NewPutEndpointIDHandler(d *Daemon) PutEndpointIDHandler {
 // The returned pod is deepcopied which means the its fields can be written
 // into.
 func (d *Daemon) fetchK8sMetadataForEndpoint(nsName, podName string) (*slim_corev1.Pod, []slim_corev1.ContainerPort, labels.Labels, labels.Labels, map[string]string, error) {
-	p, err := d.k8sWatcher.GetCachedPod(nsName, podName)
-	if err != nil {
-		return nil, nil, nil, nil, nil, err
-	}
-	ns, err := d.k8sWatcher.GetCachedNamespace(nsName)
+	ns, p, err := d.endpointMetadataFetcher.Fetch(nsName, podName)
 	if err != nil {
 		return nil, nil, nil, nil, nil, err
 	}
@@ -187,6 +186,38 @@ func (d *Daemon) fetchK8sMetadataForEndpoint(nsName, podName string) (*slim_core
 	k8sLbls := labels.Map2Labels(lbls, labels.LabelSourceK8s)
 	identityLabels, infoLabels := labelsfilter.Filter(k8sLbls)
 	return p, containerPorts, identityLabels, infoLabels, annotations, nil
+}
+
+type cachedEndpointMetadataFetcher struct {
+	k8sWatcher *watchers.K8sWatcher
+}
+
+func (cemf *cachedEndpointMetadataFetcher) Fetch(nsName, podName string) (*slim_corev1.Namespace, *slim_corev1.Pod, error) {
+	p, err := cemf.k8sWatcher.GetCachedPod(nsName, podName)
+	if err != nil {
+		return nil, nil, err
+	}
+	ns, err := cemf.k8sWatcher.GetCachedNamespace(nsName)
+	if err != nil {
+		return nil, nil, err
+	}
+	return ns, p, err
+}
+
+type uncachedEndpointMetadataFetcher struct {
+	slimcli slimclientset.Interface
+}
+
+func (uemf *uncachedEndpointMetadataFetcher) Fetch(nsName, podName string) (*slim_corev1.Namespace, *slim_corev1.Pod, error) {
+	p, err := uemf.slimcli.CoreV1().Pods(nsName).Get(context.TODO(), podName, metav1.GetOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+	ns, err := uemf.slimcli.CoreV1().Namespaces().Get(context.TODO(), nsName, metav1.GetOptions{})
+	if err != nil {
+		return nil, nil, err
+	}
+	return ns, p, err
 }
 
 func invalidDataError(ep *endpoint.Endpoint, err error) (*endpoint.Endpoint, int, error) {

daemon/cmd/daemon.go
@@ -57,6 +57,7 @@ import (
 	"github.com/cilium/cilium/pkg/k8s"
 	k8sConst "github.com/cilium/cilium/pkg/k8s/apis/cilium.io"
 	k8sClient "github.com/cilium/cilium/pkg/k8s/client"
+	slim_corev1 "github.com/cilium/cilium/pkg/k8s/slim/k8s/api/core/v1"
 	"github.com/cilium/cilium/pkg/k8s/watchers"
 	"github.com/cilium/cilium/pkg/labels"
 	"github.com/cilium/cilium/pkg/lock"
@@ -166,6 +167,9 @@ type Daemon struct {
 	ipcache *ipcache.IPCache
 
 	k8sWatcher *watchers.K8sWatcher
+
+	// endpointMetadataFetcher knows how to fetch Kubernetes metadata for endpoints.
+	endpointMetadataFetcher endpointMetadataFetcher
 
 	// healthEndpointRouting is the information required to set up the health
 	// endpoint's routing in ENI or Azure IPAM mode
@@ -1486,4 +1490,8 @@ func (d *Daemon) K8sCacheIsSynced() bool {
 	default:
 		return false
 	}
+}
+
+type endpointMetadataFetcher interface {
+	Fetch(nsName, podName string) (*slim_corev1.Namespace, *slim_corev1.Pod, error)
 }

daemon/cmd/state.go
@@ -159,6 +159,14 @@ func (d *Daemon) restoreOldEndpoints(state *endpointRestoreState, clean bool) er
 		return nil
 	}
 
+	var emf endpointMetadataFetcher
+	if option.Config.EnableHighScaleIPcache {
+		emf = &uncachedEndpointMetadataFetcher{slimcli: k8s.WatcherClient()}
+	} else {
+		emf = &cachedEndpointMetadataFetcher{k8sWatcher: d.k8sWatcher}
+	}
+	d.endpointMetadataFetcher = emf
+
 	log.Info("Restoring endpoints...")
 
 	var (

commit 6af11b58436cb39442e765989329d2a5e7362d79

daemon: Fix use of GetCachedPod with high-scale IPcache
Commit bdb1b6308 ("daemon: Introduce endpointMetadataFetcher")
introduced an alternative implementation to GetCachedPod for when the
high-scale IPcache is enabled and the pod watcher and pod store are
disabled.

There are however a few places that were missed and may still rely on
the GetCachedPod method when high-scale IPcache is enabled. This commit
replaces them with the Fetch method (which abstracts both
implementations).

Fixes: bdb1b6308 ("daemon: Introduce endpointMetadataFetcher")
Signed-off-by: Paul Chaignon <paul@cilium.io>

daemon/cmd/endpoint.go
@@ -479,21 +479,21 @@ func (d *Daemon) createEndpoint(ctx context.Context, owner regeneration.Owner, e
 	// manager creates the endpoint queue the operation will fail.
 	if ep.K8sNamespaceAndPodNameIsSet() && k8s.IsEnabled() && k8sLabelsConfigured {
 		ep.UpdateVisibilityPolicy(func(ns, podName string) (proxyVisibility string, err error) {
-			p, err := d.k8sWatcher.GetCachedPod(ns, podName)
+			_, p, err := d.endpointMetadataFetcher.Fetch(ns, podName)
 			if err != nil {
 				return "", err
 			}
 			return p.Annotations[annotation.ProxyVisibility], nil
 		})
 		ep.UpdateBandwidthPolicy(func(ns, podName string) (bandwidthEgress string, err error) {
-			p, err := d.k8sWatcher.GetCachedPod(ns, podName)
+			_, p, err := d.endpointMetadataFetcher.Fetch(ns, podName)
 			if err != nil {
 				return "", err
 			}
 			return p.Annotations[bandwidth.EgressBandwidth], nil
 		})
 		ep.UpdateNoTrackRules(func(ns, podName string) (noTrackPort string, err error) {
-			p, err := d.k8sWatcher.GetCachedPod(ns, podName)
+			_, p, err := d.endpointMetadataFetcher.Fetch(ns, podName)
 			if err != nil {
 				return "", err
 			}
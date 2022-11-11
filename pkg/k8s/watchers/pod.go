ChengyuanLiCY/cilium

commit 309edd259dbc7a1f2e70f440e7b549e1d76be0ca

k8s/watchers: Prevent creation of pod watcher with high-scale ipcache
No need for the pod watcher in the high-scale ipcache mode because we
can rely on the CEP watcher to insert into the ipcache instead. This
helps Cilium's scalability when there's high pod churn in a cluster.

Signed-off-by: Chris Tarazi <chris@isovalent.com>

pkg/k8s/watchers/pod.go
@@ -150,6 +150,12 @@ func (k *K8sWatcher) podsInit(slimClient slimclientset.Interface, asyncControlle
 		return
 	}
 
+	// Disable watching pods if we are in high-scale mode. We don't need to
+	// insert pod IPs into the ipcache.
+	if option.Config.EnableHighScaleIPcache {
+		return
+	}
+
 	// If CiliumEndpointCRD is disabled, we will fallback on watching all pods
 	// and then watching on the pods created for this node if the
 	// K8sEventHandover is enabled.
@@ -204,12 +210,6 @@ func (k *K8sWatcher) addK8sPodV1(pod *slim_corev1.Pod) error {
 		"hostIP":               pod.Status.HostIP,
 	})
 
-	// Disable watching pods if we are in high-scale mode. We don't need to
-	// insert pod IPs into the ipcache.
-	if option.Config.EnableHighScaleIPcache {
-		return nil
-	}
-
 	// In Kubernetes Jobs, Pods can be left in Kubernetes until the Job
 	// is deleted. If the Job is never deleted, Cilium will never receive a Pod
 	// delete event, causing the IP to be left in the ipcache.
@@ -247,12 +247,6 @@ func (k *K8sWatcher) addK8sPodV1(pod *slim_corev1.Pod) error {
 
 func (k *K8sWatcher) updateK8sPodV1(oldK8sPod, newK8sPod *slim_corev1.Pod) error {
 	if oldK8sPod == nil || newK8sPod == nil {
-		return nil
-	}
-
-	// Disable watching pods if we are in high-scale mode. We don't need to
-	// insert pod IPs into the ipcache.
-	if option.Config.EnableHighScaleIPcache {
 		return nil
 	}
 
@@ -711,11 +705,6 @@ func (k *K8sWatcher) updatePodHostData(oldPod, newPod *slim_corev1.Pod, oldPodIP
 
 	if newPod.Spec.HostNetwork {
 		logger.Debug("Pod is using host networking")
-		return nil
-	}
-
-	if option.Config.EnableHighScaleIPcache && nodeTypes.GetName() != newPod.Spec.NodeName {
-		logger.Debug("Pod is not local; skipping ipcache upsert")
 		return nil
 	}
 

commit ea5a9ecb1059465f2340a0bbd61a7a8344eb23da

watcher: Always disable pod watcher with high-scale IPcache
Commit 309edd259 disabled the pod watcher when high-scale IPcache is
enabled, but because of where the skip logic was added, the pod watcher
isn't disabled when disable-endpoint-crd=false. That flag is false by
default so the pod watcher wasn't disabled by default :-(

This commit fixes it by moving the skip logic before the
disable-endpoint-crd check. We also need to signal to the wait group
that we are done or the agent will be blocked on startup.

Fixes: 309edd259 ("k8s/watchers: Prevent creation of pod watcher with high-scale ipcache")
Signed-off-by: Paul Chaignon <paul@cilium.io>

pkg/k8s/watchers/pod.go
@@ -141,18 +141,19 @@ func (k *K8sWatcher) podsInit(slimClient slimclientset.Interface, asyncControlle
 		return isConnected
 	}
 
+	// Disable watching pods if we are in high-scale mode. We don't need to
+	// insert pod IPs into the ipcache.
+	if option.Config.EnableHighScaleIPcache {
+		asyncControllers.Done()
+		return
+	}
+
 	// We will watch for pods on th entire cluster to keep existing
 	// functionality untouched. If we are running with CiliumEndpoint CRD
 	// enabled then it means that we can simply watch for pods that are created
 	// for this node.
 	if !option.Config.DisableCiliumEndpointCRD {
 		watchNodePods()
-		return
-	}
-
-	// Disable watching pods if we are in high-scale mode. We don't need to
-	// insert pod IPs into the ipcache.
-	if option.Config.EnableHighScaleIPcache {
 		return
 	}
 